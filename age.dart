d3b707b6c7fe122661b4bb645096e9a85a052439
Diego Silva <41221742+odiegosilva1@users.noreply.github.com>
2021-01-03 12:44:28 -0200

    Update age.dart

--- age.dart
+++ age.dart
@@ -1,16 +1,21 @@
-//Instalando bliblioteca de leitura de inputs
+//Importando bliblioteca de leitura de inputs
 import 'dart:io';
 
 main(){
 
-var input - stdin.readLineSync();
+print("====Digite Sua idade====");
+var input = stdin.readLineSync();
 var idade = int.parse(input);
 
 
-if(idade >= 18){
-  print("maior de idade");
+  if(idade >= 50){
+    print("melhor idade");
+  } else if(idade >= 18){
+    print("Adulto");
+  } else if(idade >= 12){
+    print("adolecente");
   } else {
-  print("menor idade");
+    print("criança");
   }
 
 }
